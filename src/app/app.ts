d8066bdaf54014e4119f4171bf0d12ed98801525
OzanElicatal <ozanelctl@gmail.com>
2025-10-28 20:43:12 +0300

    feat: added movie dashboard page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MovieDashboardComponent } from './features/movie-dashboard/movie-dashboard';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [RouterOutlet, MovieDashboardComponent],
+  template: `
+    <app-movie-dashboard />
+    <router-outlet />
+  `
 })
-export class App {
-  protected readonly title = signal('movieAi');
-}
+export class App {}
---
+++ src/app/features/movie-dashboard/movie-dashboard.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-movie-dashboard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './movie-dashboard.html',
+  styleUrl: './movie-dashboard.css',
+})
+export class MovieDashboardComponent {}
